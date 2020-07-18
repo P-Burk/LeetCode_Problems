2092fc8b3d35a8bb655aba79923eca4af9233ef0
P-Burk <P-Burk@users.noreply.github.com>
2020-07-18 01:32:30 -0400

    Updated comments to question 860 to break down the script and give my thoughts

--- 860_Easy_Lemonade_Change.py
+++ 860_Easy_Lemonade_Change.py
@@ -24,38 +24,39 @@
 
 class Solution:
     def lemonadeChange(self, bills_list):
-        fives = 0
+        fives = 0                            # first 3 lines sets counters for the amount of each bill you have
         tens = 0
         twenties = 0
-        for x in bills_list:
-            print(x)
-            if x == 5:
-                fives += 1
-            elif x == 10:
-                tens += 1
-                fives = fives - 1
-                if fives >= 0:
+        for x in bills_list:                 # iterates through the list of bills
+            if x == 5:                       # if the bill is a five then....
+                fives += 1                   # +1 to $5 counter
+            elif x == 10:                    # else if bill is a 10 then....
+                tens += 1                    # +1 to $10 counter
+                fives -= 1                   # -1 from $5 counter because you give back $5 as change
+                if fives >= 0:               # you can only give back fives, so check to see if you have negative fives, if so, returns False, else continue
                     continue
                 else:
                     return False
-            elif x == 20:
-                twenties += 1
-                if tens >= 1:
-                    tens = tens - 1
-                    fives = fives - 1
-                    if fives < 0:
+            elif x == 20:                    # else if bill is a 20 then....
+                twenties += 1                # +1 to $20 counter
+                if tens >= 1:                # check to see if you have any $10 bills to use as change. if yes then....
+                    tens -= 1                # -1 from $10 counter
+                    fives -= 1               # -1 from $5 counter
+                    if fives < 0:            # checks to see if you ran out of fives. can't give negative money. if negative, returns False
                         return False
-                else:
-                    fives = fives - 3
-                    if fives < 0:
+                else:                        # if there are no $10 bills to use as change....
+                    fives -= 3               # -3 from $5 counter because you give back $15 in fives
+                    if fives < 0:            # checks to see if you ran out of fives. can't give negative money. if negative, returns False
                         return False
-#        if fives >= 0 and tens >= 0 and twenties >= 0:
-        return True
-
+        return True                          # if it makes it through the list without hitting a false, then it's True
 
 s = Solution()
-
-payment = [5, 10,10]
-
+payment = [5, 10, 10]  # False
+payment2 = [5, 5, 10, 10, 20]  # False
+payment3 = [5, 5, 10]  # True
 print(s.lemonadeChange(payment))
 
+
+# All in all, I feel like my answer is pretty rudimentary. Just a bunch of if, elif, else statements to navigate down different branching paths.
+# Some paths lead to a 'False' and only one leads to a 'True'. First answers were pretty basic and I didn't realize you could use $10 to make change
+# for the $20 payment. I thought the question was a lot easier than it actually is if I only had to make sure I didn't run out of $5 bills hahaha!
